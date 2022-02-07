48913426f6cdd6965e808be9be298b469d379366
flo-collab <f.u.etudes@laposte.net>
2022-02-07 18:31:10 +0100

    some tests added

--- immoweb/main/tests.py
+++ immoweb/main/tests.py
@@ -1,9 +1,29 @@
 from django.test import TestCase
 from django.test import Client
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 class RouteTestCase(TestCase):
+    def setUp(self):
+        self.response = self.client.get('http://127.0.0.1:8000')
+        test_user = User.objects.create_user(username='toto', password='turlututu')
+        test_user.save()
+
     def test_index_loads_properly(self):
-        # The index page loads properly
-        response = self.client.get('http://127.0.0.1:8000')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_login(self):
+        self.client.login(username='toto', password='turlututu')
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_authenticated)
+
+    def test_not_logged(self):
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+
+    def test_logout(self):
+        self.client.login(username='toto', password='turlututu')
+        self.client.logout()
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated)
     
